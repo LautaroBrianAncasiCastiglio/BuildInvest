551d3599171dd1066147af228b9748667e08a009
LautaroBrianAncasiCastiglio <lautarocas05@gmail.com>
2024-09-04 22:25:54 -0300

    aesesea

---
+++ bdPam.js
@@ -0,0 +1,10 @@
+let maps =[
+    {
+        address: string,
+        location: LatLng,
+        placeId: string,
+        bounds: LatLngBounds,
+        componentRestrictions: GeocoderComponentRestrictions,
+        region: string,
+        },
+];
