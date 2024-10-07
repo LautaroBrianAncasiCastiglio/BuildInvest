39f40fee850d3a03fff095c2d4016859cd67e3d0
lgaieta <lgaieta.dev@gmail.com>
2024-10-07 19:28:57 -0300

    Added mysql module and a MySQLPool

---
+++ src/services/MySQLPool.ts
@@ -0,0 +1,11 @@
+import mysql, { Pool } from "mysql2/promise";
+
+export const MySQLPool: Pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "1234",
+  database: process.env.DB_NAME || "buildinvest",
+  port: +process.env.DB_PORT!,
+});
+
+export default MySQLPool;
