5ff3fff5d2823ec3716505b8df439b73b54e098f
lgaieta <lgaieta.dev@gmail.com>
2024-10-15 10:53:23 -0300

    Added user login

---
+++ src/app/iniciar-sesion/page.tsx
@@ -0,0 +1,13 @@
+import LoginPage from "@/components/login/LoginPage";
+
+export const metadata = {
+    title: "Iniciar sesión - BuildInvest",
+    description:
+        "Inicia sesión para acceder a nuestra plataforma BuildInvest. Crea una cuenta para comenzar a invertir.",
+};
+
+function Login() {
+    return <LoginPage />;
+}
+
+export default Login;
