0fd73b24b6000a22d0e0d124851402991ecd0e18
lgaieta <lgaieta.dev@gmail.com>
2024-10-15 10:38:06 -0300

    Added page title in register

--- src/app/crear-cuenta/page.tsx
+++ src/app/crear-cuenta/page.tsx
@@ -1,5 +1,11 @@
 import RegisterPage from "@/components/register/RegisterPage";
 
+export const metadata = {
+    title: "Crear cuenta - BuildInvest",
+    description:
+        "Crea una cuenta para comenzar a invertir en la plataforma BuildInvest",
+};
+
 function Register() {
     return <RegisterPage />;
 }
