6f1e35aeab739538ba74c57af60db85582e1c87a
lgaieta <lgaieta.dev@gmail.com>
2024-11-14 11:01:15 -0300

    Fixed styles in Project Details

--- src/app/proyectos/detalles/[id]/page.tsx
+++ src/app/proyectos/detalles/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import type ProjectRepository from "@/models/ProjectRepository";
 import MySQLProjectRepository from "@/services/repositories/MySQLProjectRepository";
 import Link from "next/link";
@@ -10,7 +11,7 @@ async function ProjectDetails({ params }: { params: { id: string } }) {
 
     if (!project)
         return (
-            <main className="flex gap-4 justify-center items-center h-full w-full py-12 lg:py-24">
+            <main className="flex gap-4 justify-center items-center h-full w-full py-12 lg:py-24 px-4">
                 <div className="flex flex-col items-center w-full space-y-10">
                     <header className="max-w-sm flex flex-col gap-4 items-center text-center">
                         <p>El proyecto que estás buscando no existe.</p>
@@ -23,12 +24,12 @@ async function ProjectDetails({ params }: { params: { id: string } }) {
         );
 
     return (
-        <main className="flex gap-4 justify-center items-center h-full w-full py-12 lg:py-24">
-            <div className="flex flex-col items-center max-w-3xl w-full space-y-10">
-                <Card className="flex flex-col gap-4 p-8 w-full rounded-[28px]">
+        <main className="flex gap-4 justify-center items-center h-full w-full py-12 lg:py-24 px-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-start max-w-5xl w-full">
+                <Card className="flex flex-col gap-4 md:col-span-2 p-4 sm:p-8 w-full rounded-[28px]">
                     <CardHeader className="w-full">
                         <header>
-                            <h1 className="text-3xl font-bold">
+                            <h1 className="text-3xl font-bold text-primary">
                                 {project?.name}
                             </h1>
                         </header>
@@ -58,6 +59,38 @@ async function ProjectDetails({ params }: { params: { id: string } }) {
                                 {project.estimatedEndDate.toLocaleDateString()}
                             </p>
                         </div>
+                    </CardContent>
+                </Card>
+                <Card className="flex flex-col gap-4 p-2 w-full rounded-[28px]">
+                    <CardHeader className="w-full pb-0">
+                        <header>
+                            <h2 className="text-xl font-bold">
+                                Inversión actual
+                            </h2>
+                        </header>
+                    </CardHeader>
+                    <CardContent className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-1">
+                            <div className="flex w-full justify-between">
+                                <p className="text-primary font-bold">
+                                    {project.total}$
+                                </p>
+                                <p className="font-bold">
+                                    {project.maxToInvest}$
+                                </p>
+                            </div>
+                            <Progress
+                                value={
+                                    (project.total / project.maxToInvest) * 100
+                                }
+                            />
+                        </div>
+                        <Button
+                            className="w-full text-base font-bold"
+                            size={"lg"}
+                        >
+                            Invertir ahora
+                        </Button>
                     </CardContent>
                 </Card>
             </div>
