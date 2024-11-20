bbd87bb09f15bd7fefaa2fed3ebb083aa262b514
lgaieta <lgaieta.dev@gmail.com>
2024-11-20 16:59:48 +0000

    Added styles in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,14 @@ export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <main className="flex flex-col items-center">
-                <section className="flex flex-col items-center space-y-8 text-center w-full py-12 md:py-24 lg:py-32 xl:py-48 px-4">
-                    <div className="space-y-8">
-                        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-foreground">
-                            Inversiones fáciles
+                <section className="relative flex flex-col items-center w-full bg-zinc-800">
+                    <div className="max-w-4xl flex flex-col gap-4 items-start w-full py-24 lg:py-32 xl:py-40 px-4">
+                        <h1 className="text-3xl text-white max-w-md font-bold tracking-tighter sm:text-4xl text-foreground text-balance">
+                            Comenzá a invertir en proyectos inmobiliarios con
+                            sumas pequeñas
                         </h1>
-                        <p className="mx-auto max-w-[700px] text-foreground/90 text-balance md:text-lg">
-                            Comenzá a invertir en proyectos inmobiliarios sin
-                            complicaciones técnicas, no necesitás una gran suma
-                            de dinero.
-                        </p>
-                    </div>
-                    <div className="space-x-4">
-                        <Button size={"lg"} asChild>
-                            <Link href="/proyectos">Invertir ahora</Link>
+                        <Button size={"lg"} asChild className="font-bold">
+                            <Link href="/proyectos">Ver proyectos</Link>
                         </Button>
                     </div>
                 </section>
