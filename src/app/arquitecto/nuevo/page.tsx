e126d55ddfcc3dd935280d8ad508992805a2dad2
lgaieta <lgaieta.dev@gmail.com>
2024-11-27 15:32:36 +0000

    Added architect user validation when tries to register as architect

--- src/app/arquitecto/nuevo/page.tsx
+++ src/app/arquitecto/nuevo/page.tsx
@@ -1,10 +1,12 @@
 import NewArchitectPage from "@/components/new-architect/NewArchitectPage";
+import { UserType } from "@/models/User";
 import SessionManager from "@/services/SessionManager";
 import { redirect } from "next/navigation";
 
 async function NewArchitect() {
-    const { isAuth } = await SessionManager.verifySession();
+    const { isAuth, usertype } = await SessionManager.verifySession();
     if (!isAuth) redirect("/iniciar-sesion");
+    if (usertype === UserType.architect) redirect("/cuenta/arquitecto");
     return <NewArchitectPage />;
 }
 
